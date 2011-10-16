78fd936f632464024d3be113c5914137b33651a9
shorinnn <shorinnn@yahoo.com>
2011-10-16 15:06:42 +0300

    Done with user edit, index and destroy

--- Gemfile
+++ Gemfile
@@ -3,8 +3,10 @@ source 'http://rubygems.org'
 gem 'rails', '3.1.1.rc1'
 gem 'sqlite3', :group => :development
 gem 'gravatar_image_tag', '1.0.0.pre2'
+gem 'will_paginate', '3.0.pre2'
 
 group :development do
+  gem 'faker', '0.3.1'
   gem 'rspec-rails', '2.6.1'
   gem 'annotate', 
 :git => 'git://github.com/jeremyolliver/annotate_models.git', :branch => 'rake_compatibility'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
+  before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user, :only => :destroy
+  
+  def index
+    @users = User.paginate(:page => params[:page])
+    @title = "All users"
+  end
+  
   def new
     @user = User.new
     @title = 'Sign Up'
@@ -20,5 +29,39 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-# does this feel slow to you?
+  
+  def edit
+    @title = 'Edit user'
+    
+  end
+  
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed"
+    redirect_to users_path
+  end
+
+  private
+  def authenticate
+      deny_access unless signed_in?
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path unless current_user?@user
+  end
+  
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
 end
