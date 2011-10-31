18fef07151d50ad82c8a1e7789eaa74719e7c9d5
shorinnn <shorinnn@yahoo.com>
2011-10-31 18:55:02 +0200

    Added user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
+  before_filter :authenticate, :except => [:show, :new, :create]
   before_filter :correct_user, :only => [:edit, :update]
   before_filter :admin_user, :only => :destroy
   before_filter :logged_user, :only =>[:new, :create]
@@ -34,7 +34,6 @@ class UsersController < ApplicationController
   
   def edit
     @title = 'Edit user'
-    
   end
   
   def update
@@ -56,6 +55,20 @@ class UsersController < ApplicationController
       flash[:error] = "Can't delete yourself"
       redirect_to users_path 
     end
+  end
+  
+  def following
+    @title = 'Following'
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(:page => params[:page])
+    render "show_follow"
+  end
+  
+  def followers
+    @title = 'Followers'
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(:page => params[:page])
+    render "show_follow"
   end
 
   private
